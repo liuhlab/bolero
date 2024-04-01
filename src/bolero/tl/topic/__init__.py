8476f9cd1c797ef5743ca803a57c0a79a64d6fb6
hq <liuhq@ucsd.edu>
2024-04-01 09:41:54 -0400

    Update __init__.py

--- src/bolero/tl/topic/__init__.py
+++ src/bolero/tl/topic/__init__.py
@@ -1,3 +1,6 @@
-from .lda import LDAMallet, train_lda, CistopicLDAModel
-from .evaluate import evaluate_models
 from .binarize_topic import binarize_topics
+from .evaluate import evaluate_models
+from .lda import CistopicLDAModel, LDAMallet, train_lda
+
+
+# TODO delect this code and use scmallet
