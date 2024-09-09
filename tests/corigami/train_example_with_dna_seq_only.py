cf55961e06d2666575ae0c7fabc141d6c92ca4de
Yishuang Chen <yishuangchen17@gmail.com>
2024-09-09 10:59:41 -0700

    small changes (#39)
    Add a test script for training on dna seq only
    Add wandb_name into config

--- tests/corigami/train_example_with_dna_seq_only.py
+++ tests/corigami/train_example_with_dna_seq_only.py
@@ -4,24 +4,19 @@ from glob import glob
 
 from bolero.tl.generic.train_helper import corigami_hg38_splits
 from bolero import init
-from bolero.tl.model.corigami.train import CorigamiTrainer
+from bolero.tl.model.corigami.train import CorigamiSeqOnlyTrainer
 #%%
 init(num_cpus=32, object_store_memory_gb=400, verbose=True)
 
 #%%
 indir = '/large_storage/zhoulab/project/seqmodel/data/corigami/corigami_data/data/hg38/imr90/hic_matrix/'
 cool_paths = np.sort(glob(f'{indir}*.cool'))
-
-#%%
-atac_paths = ['/large_storage/zhoulab/project/seqmodel/data/corigami/corigami_data/data/hg38/imr90/genomic_features/atac.bw']
-ctcf_paths = ['/large_storage/zhoulab/project/seqmodel/data/corigami/corigami_data/data/hg38/imr90/genomic_features/ctcf_log2fc.bw']
-
 #%%
 config = {
     # dataset
     "cool_paths": cool_paths[:1].tolist(),
-    "atac_paths": atac_paths,
-    "ctcf_paths": ctcf_paths,
+    "atac_paths": None,
+    "ctcf_paths": None,
     "resolution": 10000,
     "balance": False,
     "genome": 'hg38',
@@ -31,12 +26,11 @@ config = {
     "bed": '/large_storage/zhoulab/project/seqmodel/data/corigami/corigami_data/data/hg38/train.bed',
     "standard_length": 2097152,
     "dna_fifth_channel": True,
-    "data_1d_keys": ("atac", "ctcf",),
+    "data_1d_keys": None,
     # model
     "image_scale": 256,
     "encoder_in_channel": 5,
-    "encoder_num_epi": 2,
-    # trainng
+    # training
     "mode": "base",
     "pretrained_model": None, # "/large_experiments/zhoulab/project/seqmodel/data/corigami/corigami_data/model_weights/corigami_base.ckpt",
     "chrom_split": corigami_hg38_splits[0],
@@ -48,16 +42,16 @@ config = {
     "lr": 0.002,
     "use_ema": False,
     # save data
-    "output_dir": "corigami_result", # NEED TO CHANGE
-    "wandb_project": "corigami_result", # NEED TO CHANGE
+    "output_dir": "corigami_09_06_ema_disabled_5_channel_dna_seq_only", # NEED TO CHANGE
+    "wandb_project": "corigami_09_06_ema_disabled_5_channel_dna_seq_only", # NEED TO CHANGE
     "wandb_job_type": "train",
-    "wandb_name": "corigami_dna_seq_atac_ctcf",
+    "wandb_name": "corigami_dna_seq_only",
     "wandb_group": None,
     "savename": "base",
 }
-config = CorigamiTrainer.make_config(**config)
+config = CorigamiSeqOnlyTrainer.make_config(**config)
 
 #%%
-trainer = CorigamiTrainer(config)
+trainer = CorigamiSeqOnlyTrainer(config)
 #%%
 trainer.train()
